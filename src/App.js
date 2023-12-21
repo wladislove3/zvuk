1062c4f38a7ec061786ea7da6f7fa8ca434a9fef
Carrie <carrieleps@gmail.com>
2023-12-22 01:23:51 +0300

    Инциализация проекта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,472 @@
-import logo from './logo.svg';
-import './App.css';
 
+import * as tf from "@tensorflow/tfjs"
+import React, { useState, useEffect, useRef } from "react";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { AudioContext } from "standardized-audio-context";
+import { Panner } from "tone";
+import { loadGraphModel, tensor, tidy, stack, unstack, squeeze, expandDims, reshape, slice, concat, reverse, cast, gather, split, matMul, add, sub, mul, div, pow, abs, round, exp, log, sqrt, sin, cos, tan, asin, acos, atan, sinh, cosh, tanh, asinh, acosh, atanh, erf, step, relu, elu, selu, leakyRelu, prelu, sigmoid, logSigmoid, softplus, zeros, ones, fill, linspace, range, buffer, variable, onesLike, zerosLike, fillLike, linspaceLike, rangeLike, bufferLike, clone, oneHot, fromPixels, toPixels, print, memory, time, nextFrame } from "@tensorflow/tfjs"
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+// Инициализируем Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD-7wYlZ4J0Qf5a7y6Qxg9c3kZ1Xy6Wf0o",
+  authDomain: "zvuk-ad2e8.firebaseapp.com",
+  projectId: "zvuk-ad2e8",
+  storageBucket: "zvuk-ad2e8.appspot.com",
+  messagingSenderId: "1047254549200",
+  appId: "1:1047254549200:web:7c9b8f9a2c0c0d1a0a0f0a",
+  measurementId: "G-0F8LQ0VZ0E"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Получаем ссылки на сервисы Firebase
+const firestore = getFirestore (firebaseApp);
+const auth = getAuth (firebaseApp);
+const storage = getStorage (firebaseApp);
+
+
+// Создаем компонент App, который sбудет отображать основной интерфейс приложения
 function App() {
+  // Создаем состояния для хранения аудиофайлов, анализа, микса и визуализации
+  const [audioFiles, setAudioFiles] = useState([]);
+  const [analysis, setAnalysis] = useState(null);
+  const [mix, setMix] = useState(null);
+  const [visuals, setVisuals] = useState(null);
+
+  // Создаем ссылки на элементы canvas и audio
+  const canvasRef = useRef(null);
+  const audioRef = useRef(null);
+
+  // Создаем объекты для работы с аудиоданными
+  const audioContext = new AudioContext();
+  const analyser = audioContext.createAnalyser();
+  const panner = new Panner();
+
+  // Создаем объекты для работы с моделями машинного обучения
+  const instrumentClassifier = useRef(null);
+  const trackSeparator = useRef(null);
+  const harmonyGenerator = useRef(null);
+  const mixOptimizer = useRef(null);
+
+  // Создаем объект для работы с Firebase
+  const firebaseConfig = {
+    // Здесь нужно указать свои настройки Firebase
+  };
+
+  // Загружаем модели машинного обучения при монтировании компонента
+  useEffect(() => {
+    (async () => {
+      instrumentClassifier.current = await tf.loadLayersModel(
+        "https://example.com/instrument-classifier/model.json"
+      );
+      trackSeparator.current = await tf.loadGraphModel(
+        "https://example.com/track-separator/model.json"
+      );
+      harmonyGenerator.current = await tf.loadGraphModel(
+        "https://example.com/harmony-generator/model.json"
+      );
+      mixOptimizer.current = await tf.loadGraphModel(
+        "https://example.com/mix-optimizer/model.json"
+      );
+    })();
+  }, []);
+
+  // Обрабатываем событие выбора аудиофайлов
+  const handleFileChange = (e) => {
+    // Получаем список выбранных файлов
+    const files = e.target.files;
+
+    // Создаем массив для хранения информации о файлах
+    const audioFiles = [];
+
+    // Проходим по каждому файлу
+    for (let i = 0; i < files.length; i++) {
+      // Получаем имя, размер и тип файла
+      const name = files[i].name;
+      const size = files[i].size;
+      const type = files[i].type;
+
+      // Проверяем, что файл является аудиофайлом
+      if (type.startsWith("audio/")) {
+        // Создаем объект URL для файла
+        const url = URL.createObjectURL(files[i]);
+
+        // Добавляем информацию о файле в массив
+        audioFiles.push({ name, size, type, url });
+      }
+    }
+
+    // Обновляем состояние аудиофайлов
+    setAudioFiles(audioFiles);
+  };
+
+  // Обрабатываем событие анализа аудиофайлов
+  const handleAnalyzeClick = async () => {
+    // Проверяем, что есть выбранные аудиофайлы
+    if (audioFiles.length > 0) {
+      // Создаем объект для хранения результата анализа
+      const analysis = {};
+
+      // Проходим по каждому аудиофайлу
+      for (let i = 0; i < audioFiles.length; i++) {
+        // Получаем имя и URL файла
+        const name = audioFiles[i].name;
+        const url = audioFiles[i].url;
+
+        // Загружаем аудиоданные из URL
+        const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+
+        // Конвертируем аудиоданные в тензор
+        const audioTensor = tf.tensor(audioBuffer.getChannelData(0));
+
+        // Используем модель для классификации инструмента
+        const instrumentPrediction = instrumentClassifier.current.predict(
+          audioTensor
+        );
+        const instrumentIndex = instrumentPrediction.argMax().dataSync()[0];
+        const instrumentLabels = [
+          "Acoustic Guitar",
+          "Bass Guitar",
+          "Drums",
+          "Electric Guitar",
+          "Piano",
+          "Saxophone",
+          "Violin",
+          "Vocals",
+        ];
+        const instrument = instrumentLabels[instrumentIndex];
+
+        // Используем модель для сегментации аудиодорожки
+        const trackSegments = trackSeparator.current.execute(
+          audioTensor.expandDims(0)
+        );
+        const trackLabels = ["Bass", "Drums", "Harmony", "Melody", "Vocals"];
+        const trackComponents = {};
+        for (let j = 0; j < trackSegments.length; j++) {
+          trackComponents[trackLabels[j]] = trackSegments[j].squeeze().dataSync();
+        }
+
+        // Добавляем результат анализа для файла в объект
+        analysis[name] = { instrument, trackComponents };
+      }
+
+      // Обновляем состояние анализа
+      setAnalysis(analysis);
+    }
+  };
+
+  // Обрабатываем событие микширования аудиофайлов
+  const handleMixClick = async () => {
+    // Проверяем, что есть результат анализа
+    if (analysis) {
+      // Создаем объект для хранения результата микширования
+      const mix = {};
+
+      // Проходим по каждому аудиофайлу
+      for (let name in analysis) {
+        // Получаем инструмент и компоненты аудиодорожки
+        const instrument = analysis[name].instrument;
+        const trackComponents = analysis[name].trackComponents;
+
+        // Создаем тензоры для компонентов аудиодорожки
+        const bassTensor = tf.tensor(trackComponents["Bass"]);
+        const drumsTensor = tf.tensor(trackComponents["Drums"]);
+        const harmonyTensor = tf.tensor(trackComponents["Harmony"]);
+        const melodyTensor = tf.tensor(trackComponents["Melody"]);
+        const vocalsTensor = tf.tensor(trackComponents["Vocals"]);
+
+        // Используем модель для генерации гармонии
+        const harmonyPrediction = harmonyGenerator.current.execute(
+          melodyTensor.expandDims(0)
+        );
+        const harmonyIndex = harmonyPrediction.argMax().dataSync()[0];
+        const harmonyLabels = [
+          "Major",
+          "Minor",
+          "Diminished",
+          "Augmented",
+          "Dominant",
+          "Suspended",
+        ];
+        const harmony = harmonyLabels[harmonyIndex];
+
+        // Используем модель для оптимизации микса
+        const mixPrediction = mixOptimizer.current.execute([
+          bassTensor.expandDims(0),
+          drumsTensor.expandDims(0),
+          harmonyTensor.expandDims(0),
+          melodyTensor.expandDims(0),
+          vocalsTensor.expandDims(0),
+        ]);
+        const mixTensor = mixPrediction.squeeze();
+        const mixData = mixTensor.dataSync();
+
+        // Добавляем результат микширования для файла в объект
+        mix[name] = { instrument, harmony, mixData };
+      }
+
+      // Обновляем состояние микса
+      setMix(mix);
+    }
+  };
+
+  // Обрабатываем событие визуализации аудиофайлов
+  const handleVisualizeClick = () => {
+    // Проверяем, что есть результат микса
+    if (mix) {
+      // Создаем объект для хранения результата визуализации
+      const visuals = {};
+
+      // Проходим по каждому аудиофайлу
+      for (let name in mix) {
+        // Получаем инструмент, гармонию и данные микса
+        const instrument = mix[name].instrument;
+        const harmony = mix[name].harmony;
+        const mixData = mix[name].mixData;
+
+                // Создаем аудиобуфер из данных микса
+                const audioBuffer = audioContext.createBuffer(
+                  1,
+                  mixData.length,
+                  audioContext.sampleRate
+                );
+                audioBuffer.copyToChannel(mixData, 0);
+        
+                // Создаем объект URL для аудиобуфера
+                const url = URL.createObjectURL(
+                  new Blob([audioBuffer], { type: "audio/wav" })
+                );
+        
+                // Создаем объект для хранения параметров визуализации
+                const visualParams = {
+                  // Здесь можно указать разные параметры для визуализации, например, цвет, форма, анимация и т.д.
+                };
+        
+                // Добавляем результат визуализации для файла в объект
+                visuals[name] = { instrument, harmony, url, visualParams };
+              }
+        
+              // Обновляем состояние визуализации
+              setVisuals(visuals);
+            }
+          };
+        
+          // Обрабатываем событие сохранения аудиофайлов
+          const handleSaveClick = async () => {
+            // Проверяем, что есть результат визуализации
+            if (visuals) {
+              // Проверяем, что пользователь авторизован
+              if (auth.currentUser) {
+                // Получаем идентификатор пользователя
+                const userId = auth.currentUser.uid;
+        
+                // Проходим по каждому аудиофайлу
+                for (let name in visuals) {
+                  // Получаем URL файла
+                  const url = visuals[name].url;
+        
+                  // Загружаем файл в облако
+                  const ref = storage.ref().child(`${userId}/${name}`);
+                  const snapshot = await ref.put(url);
+                  const downloadUrl = await snapshot.ref.getDownloadURL();
+        
+                  // Добавляем информацию о файле в базу данных
+                  // Здесь нужно указать свою логику для работы с базой данных
+                }
+              } else {
+                // Предлагаем пользователю зарегистрироваться или войти
+                // Здесь нужно указать свою логику для аутентификации
+              }
+            }
+          };
+        
+          // Обрабатываем событие загрузки аудиофайлов
+          const handleLoadClick = async () => {
+            // Проверяем, что пользователь авторизован
+            if (auth.currentUser) {
+              // Получаем идентификатор пользователя
+              const userId = auth.currentUser.uid;
+        
+              // Получаем список файлов из облака
+              const list = await storage.ref().child(userId).listAll();
+        
+              // Создаем массив для хранения информации о файлах
+              const audioFiles = [];
+        
+              // Проходим по каждому файлу
+              for (let i = 0; i < list.items.length; i++) {
+                // Получаем имя, размер и тип файла
+                const name = list.items[i].name;
+                const size = (await list.items[i].getMetadata()).size;
+                const type = (await list.items[i].getMetadata()).contentType;
+        
+                // Получаем URL файла
+                const url = await list.items[i].getDownloadURL();
+        
+                // Добавляем информацию о файле в массив
+                audioFiles.push({ name, size, type, url });
+              }
+        
+              // Обновляем состояние аудиофайлов
+              setAudioFiles(audioFiles);
+            } else {
+              // Предлагаем пользователю зарегистрироваться или войти
+              // Здесь нужно указать свою логику для аутентификации
+            }
+          };
+        
+          // Обрабатываем событие воспроизведения аудиофайлов
+          const handlePlayClick = () => {
+            // Проверяем, что есть результат визуализации
+            if (visuals) {
+              // Получаем элементы canvas и audio
+              const canvas = canvasRef.current;
+              const audio = audioRef.current;
+        
+              // Получаем контекст для рисования на canvas
+              const ctx = canvas.getContext("2d");
+        
+              // Очищаем canvas
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+              // Проходим по каждому аудиофайлу
+              for (let name in visuals) {
+                // Получаем инструмент, гармонию, URL и параметры визуализации
+                const instrument = visuals[name].instrument;
+                const harmony = visuals[name].harmony;
+                const url = visuals[name].url;
+                const visualParams = visuals[name].visualParams;
+        
+                // Создаем аудиоисточник из URL
+                const source = audioContext.createMediaElementSource(url);
+        
+                // Подключаем аудиоисточник к анализатору и панораме
+                source.connect(analyser);
+                source.connect(panner);
+        
+                // Подключаем анализатор и панораму к выходу
+                analyser.connect(audioContext.destination);
+                panner.connect(audioContext.destination);
+        
+                // Задаем параметры анализатора
+                analyser.fftSize = 256;
+                const bufferLength = analyser.frequencyBinCount;
+                const dataArray = new Uint8Array(bufferLength);
+        
+                // Задаем параметры панорамы
+                panner.pan.value = Math.random() * 2 - 1;
+        
+                // Рисуем визуализацию на canvas
+                const draw = () => {
+                  // Получаем данные анализатора
+                  analyser.getByteFrequencyData(dataArray);
+        
+                  // Рисуем фон
+                  ctx.fillStyle = "black";
+                  ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+                  // Рисуем спектрограмму
+                  const barWidth = (canvas.width / bufferLength) * 2.5;
+                  let x = 0;
+                  for (let i = 0; i < bufferLength; i++) {
+                    const barHeight = dataArray[i] / 2;
+        
+                    // Задаем цвет в зависимости от инструмента и гармонии
+                    // Здесь можно использовать разные цветовые схемы
+                    let hue = 0;
+                    switch (instrument) {
+                      case "Acoustic Guitar":
+                        hue = 30;
+                        break;
+                      case "Bass Guitar":
+                        hue = 60;
+                        break;
+                      case "Drums":
+                        hue = 90;
+                        break;
+                      case "Electric Guitar":
+                        hue = 120;
+                        break;
+                      case "Piano":
+                        hue = 150;
+                        break;
+                      case "Saxophone":
+                        hue = 180;
+                        break;
+                      case "Violin":
+                        hue = 210;
+                        break;
+                      case "Vocals":
+                        hue = 240;
+                        break;
+                    }
+                    let saturation = 100;
+                    switch (harmony) {
+                      case "Major":
+                        saturation = 100;
+                        break;
+                      case "Minor":
+                        saturation = 80;
+                        break;
+                      case "Diminished":
+                        saturation = 60;
+                        break;
+                      case "Augmented":
+                        saturation = 40;
+                        break;
+                      case "Dominant":
+                        saturation = 20;
+                        break;
+                      case "Suspended":
+                        saturation = 0;
+                        break;
+                    }
+                    let lightness = 50;
+                    ctx.fillStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        
+                    // Рисуем столбик
+                    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        
+                    // Переходим к следующему столбику
+                    x += barWidth + 1;
+                  }
+        
+                  // Запрашиваем анимацию
+                  requestAnimationFrame(draw);
+                };
+        
+                // Запускаем анимацию
+                draw();
+        
+                       // Воспроизводим аудио
+        audio.src = url;
+        audio.play();
+      }
+    }
+  };
+
+  // Возвращаем JSX-разметку для компонента App
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Веб-приложение для анализа и микширования аудиодорожек инструментов с помощью AI</h1>
+      <input type="file" multiple onChange={handleFileChange} />
+      <button onClick={handleAnalyzeClick}>Анализировать</button>
+      <button onClick={handleMixClick}>Микшировать</button>
+      <button onClick={handleVisualizeClick}>Визуализировать</button>
+      <button onClick={handleSaveClick}>Сохранить</button>
+      <button onClick={handleLoadClick}>Загрузить</button>
+      <button onClick={handlePlayClick}>Воспроизвести</button>
+      <canvas ref={canvasRef} width="800" height="600"></canvas>
+      <audio ref={audioRef} controls></audio>
     </div>
   );
 }
 
+// Экспортируем компонент App
 export default App;
